5318f629617c0b85eb88e8ceede2b3520c38a54b
Brahim <32096282+bra1438@users.noreply.github.com>
2023-07-01 20:51:24 +0200

    Delete 4_✨_About.py

--- pages/4_✨_About.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def about_section():
-    st.set_page_config(page_title="About", page_icon="https://github.com/AnasMations/StudySync/blob/36ae1cad78544b8a07239d1cefa141a59f6305c8/img/icon.png?raw=true")
-
-about_section()
