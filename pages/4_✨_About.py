a64f9869f349b67d7a83a32f9d7c8c0bc51d50ba
Brahim <32096282+bra1438@users.noreply.github.com>
2023-07-01 20:49:27 +0200

    Update 4_✨_About.py

--- pages/4_✨_About.py
+++ pages/4_✨_About.py
@@ -2,12 +2,5 @@ import streamlit as st
 
 def about_section():
     st.set_page_config(page_title="About", page_icon="https://github.com/AnasMations/StudySync/blob/36ae1cad78544b8a07239d1cefa141a59f6305c8/img/icon.png?raw=true")
-    st.markdown(
-        """
-        <div style="background-color:#f9f9f9;padding:20px;border-radius:10px;">
-            <h1 style="text-align:center;color:#333;">Meet the team!</h1>
-            <p style="text-align:center;font-size:18px;color:#666;">
-                LAILA DEVELOP THIS APP TO HELP STUDENT .
-            </p>
-            
-)
+
+about_section()
